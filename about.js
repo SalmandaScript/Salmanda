a6a5c2e6821fc277d35ecf14e6ee16437d81ee9d
SalmandaScript <110246267+SalmandaScript@users.noreply.github.com>
2023-11-11 09:31:48 +0200

    Update about.js

--- about.js
+++ about.js
@@ -1,4 +1,4 @@
-document.getElementById("demo").innerHTML = "Greetings my name is Salmanda, a'm an Artists and a Computer Engineer, A'm developing programmes  with Love_ for  you and Our beloved motherland AFRICA.";
+document.getElementById("demo").innerHTML = "Greetings my name is Salmanda, a'm an Artists and a Computer Engineer, A'm developing programmes  with Love_ for  you and Our beloved Motherland AFRICA.";
       
       
  document.getElementById  ("demo0").innerHTML="To My Family.";
